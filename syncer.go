e7c905f778963644b4e82b2492d563bf91bcd80e
Benjamin Legrand <dev@legrand.ws>
2015-06-19 08:53:54 +0200

    The jobs depend on each other so keep trac of those dependencies

--- syncer.go
+++ syncer.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"mime"
 	"path"
@@ -55,18 +54,19 @@ func (s *Syncer) Sync(syncer SyncPlugin) {
 	}
 
 	DEBUG.Println("Syncing with", syncer.Name())
-	s.browseEntryList[real_path] = &BrowseEntry{real_path: real_path, folder_name: "/"}
+	main_entry := &BrowseEntry{real_path: real_path, folder_name: "/"}
+	s.browseEntryList[real_path] = main_entry
 	for _, file := range fileList {
 		if file.IsDir() {
-			entry := BrowseEntry{real_path: path.Join(real_path, file.Name()), folder_name: path.Join("/", file.Name())}
+			entry := BrowseEntry{real_path: path.Join(real_path, file.Name()), folder_name: path.Join("/", file.Name()), parent: main_entry}
 			s.browseEntryList[entry.real_path] = &entry
 		}
 	}
 	nbWorkers := 4
 	nbRunningJobs := 0
 	i := 0
-	jobChan := make(chan BrowseEntry, nbWorkers)
-	resultChan := make(chan BrowseEntry)
+	jobChan := make(chan *BrowseEntry, nbWorkers)
+	resultChan := make(chan *BrowseEntry)
 	for i < nbWorkers {
 		i = i + 1
 		go syncWorker(syncer, jobChan, resultChan)
@@ -75,40 +75,45 @@ func (s *Syncer) Sync(syncer SyncPlugin) {
 			//Set entry status to 1 for PENDING
 			entry.status = 1
 			INFO.Println("Pushing new job ", *entry)
-			jobChan <- *entry
+			jobChan <- entry
 			nbRunningJobs = nbRunningJobs + 1
 		}
 	}
 	for {
-		pending, nextEntry := s.hasPendingJobs()
-		if nil != nextEntry && nbRunningJobs < nbWorkers {
-			nextEntry.status = 1
-			INFO.Println("Pushing new job ", *nextEntry)
-			jobChan <- *nextEntry
-			nbRunningJobs = nbRunningJobs + 1
+		//Read the Job done channel
+		result := <-resultChan
+		if 0 == result.status {
+			//We received a new job to do add it to the list
+			_, ok := s.browseEntryList[result.real_path]
+			if !ok {
+				INFO.Println("Adding new job to list", result.real_path)
+				s.browseEntryList[result.real_path] = result
+			} else {
+				DEBUG.Println("Job already in list of jobs to do")
+			}
+
 			continue
 		}
-		if !pending {
-			close(jobChan)
-			break
-		} else {
-			result := <-resultChan
-			if 0 == result.status {
-				//Check if entry is already in the list
-				_, ok := s.browseEntryList[result.real_path]
-				if !ok {
-					INFO.Println("Adding new job to list", result.real_path)
-					s.browseEntryList[result.real_path] = &result
-				} else {
-					DEBUG.Println("Job already in list of jobs to do")
-				}
+		//This means the job was finished
+		nbRunningJobs = nbRunningJobs - 1
+		//Now that we have a job done, try look if we can start some new workers
+
+		for {
+			pending, nextEntry := s.hasPendingJobs()
+			if nil != nextEntry && nbRunningJobs < nbWorkers {
+				nextEntry.status = 1
+				INFO.Println("Pushing new job ", *nextEntry)
+				jobChan <- nextEntry
+				nbRunningJobs = nbRunningJobs + 1
+				continue
+			} else if !pending {
+				close(jobChan)
+				return
 			} else {
-				//The job is done
-				entry := s.browseEntryList[result.real_path]
-				entry.status = 2
-				nbRunningJobs = nbRunningJobs - 1
+				break
 			}
 		}
+
 	}
 }
 
@@ -116,6 +121,7 @@ type BrowseEntry struct {
 	real_path   string
 	folder_name string
 	status      int
+	parent      *BrowseEntry
 }
 
 func (s *Syncer) hasPendingJobs() (pending bool, next *BrowseEntry) {
@@ -124,36 +130,56 @@ func (s *Syncer) hasPendingJobs() (pending bool, next *BrowseEntry) {
 		switch entry.status {
 		case 0:
 			//This entry is not being dealt with
-			return true, entry
+			if nil == entry.parent || 2 == entry.parent.status {
+				return true, entry
+			} else if 3 == entry.parent.status {
+
+			} else {
+				pending = true
+			}
 		case 1:
 			//This entry is being dealt with
 			pending = true
+		case 2:
+		case 3:
+
 		}
 	}
 	return pending, nil
 }
 
-func syncWorker(syncer SyncPlugin, jobChan <-chan BrowseEntry, newJobChan chan<- BrowseEntry) {
+func syncWorker(syncer SyncPlugin, jobChan <-chan *BrowseEntry, newJobChan chan<- *BrowseEntry) {
 	for job := range jobChan {
 		real_path := job.real_path
 		folder := job.folder_name
 
 		//Check if this folder exists, other wise create it
 		//Exclude the / folder which shouldn't be created
-		if "/" != folder && !syncer.HasFolder(folder) {
+		if !syncer.HasFolder(folder) {
 			base := filepath.Base(folder)
 			parent := filepath.Dir(folder)
 			var entry = SyncResourceInfo{Name: base, Parent: parent, IsDir: true, Path: real_path}
-			syncer.AddResource(&entry)
+			err := syncer.AddResource(&entry)
+			if nil != err {
+				ERROR.Print("Failed to add resource ", err)
+				job.status = 3
+				newJobChan <- job
+				return
+			}
 		}
 
 		fileList, err := ioutil.ReadDir(real_path)
 		if nil != err {
 			ERROR.Print("Something went wrong ", err)
+			job.status = 3
+			newJobChan <- job
+			return
 		}
 		err, entries := syncer.BrowseFolder(folder)
 		if err != nil {
 			ERROR.Println("Failed to browse entries ", err)
+			job.status = 3
+			newJobChan <- job
 			return
 		}
 		//os.Exit(1)
@@ -173,16 +199,19 @@ func syncWorker(syncer SyncPlugin, jobChan <-chan BrowseEntry, newJobChan chan<-
 				}
 				folder_path += entry.Name
 				if !syncer.HasFolder(folder_path) {
+					INFO.Printf("Creating new folder %s", folder_path)
 					err = syncer.AddResource(&entry)
 					if err != nil {
-						fmt.Println("Failed to create Resource folder ", folder_path, " with error ", err)
+						ERROR.Println("Failed to create Resource folder ", folder_path, " with error ", err)
 						//Skip this folder, since we couldn't create the folder itself
 						continue
 					}
+				} else {
+					INFO.Printf("Folder already exists adding it to the list of sub folder to handle")
 				}
-				browseEntry := BrowseEntry{real_path: real_path + "/" + entry.Name, folder_name: folder_path}
+				browseEntry := BrowseEntry{real_path: path.Join(real_path, entry.Name), folder_name: folder_path, parent: job}
 				//Send message that a new folder has been encountered
-				newJobChan <- browseEntry
+				newJobChan <- &browseEntry
 			} else {
 				mimeType := mime.TypeByExtension("." + extension)
 				if !(strings.HasPrefix(mimeType, "image") || strings.HasPrefix(mimeType, "video")) {
@@ -199,12 +228,16 @@ func syncWorker(syncer SyncPlugin, jobChan <-chan BrowseEntry, newJobChan chan<-
 				}
 				if !found {
 					err = syncer.AddResource(&entry)
+					INFO.Printf("Adding new Resource %s\n", entry.Name)
 					if err != nil {
-						fmt.Println("Failed to add entry ", entry.Name, " with error ", err)
+						ERROR.Println("Failed to add entry ", entry.Name, " with error ", err)
 					}
+				} else {
+					INFO.Printf("%s already exists\n", entry.Name)
 				}
 			}
 		}
+		INFO.Printf("Done with the work")
 		//Notify the syncer that our job is done, and wait now for new inputs
 		job.status = 2
 		INFO.Printf("Job %s is done sending done message to main loop", real_path)
